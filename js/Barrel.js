3f5c772c778eec71361560ba06f11d05c2f3e28f
philipmetzgercsbe <philip.metzger@csbe.ch>
2019-01-25 08:48:22 +0100

    Renamed Spider to Barrel and changed inde to index

--- js/Barrel.js
+++ js/Barrel.js
@@ -1,11 +1,10 @@
-function Spider(game, x, y) {
-    Phaser.Sprite.call(this, game, x, y, 'spider');
+function Barrel(game, x, y) {
+    Phaser.Sprite.call(this, game, x, y, 'barrel');
 
     // anchor
     this.anchor.set(0.5);
     // animation
-    this.animations.add('crawl', [0, 1, 2], 8, true); // 8fps, looped
-    this.animations.add('die', [0, 4, 0, 4, 0, 4, 3, 3, 3, 3, 3, 3], 12);
+    this.animations.add('roll', [0, 1, 2], 8, true); // 8fps, looped
     this.animations.play('crawl');
 
     // physic properties
@@ -14,23 +13,23 @@ function Spider(game, x, y) {
     this.body.velocity.x = Spider.SPEED;
 }
 
-Spider.SPEED = 100;
+Barrel.SPEED = 100;
 
 // inherit from Phaser.Sprite
-Spider.prototype = Object.create(Phaser.Sprite.prototype);
-Spider.prototype.constructor = Spider;
+Barrel.prototype = Object.create(Phaser.Sprite.prototype);
+Barrel.prototype.constructor = Spider;
 
-Spider.prototype.update = function () {
+Barrel.prototype.update = function () {
     // check against walls and reverse direction if necessary
     if (this.body.touching.right || this.body.blocked.right) {
-        this.body.velocity.x = -Spider.SPEED; // turn left
+        this.body.velocity.x = -Barrel.SPEED; // turn left
     }
     else if (this.body.touching.left || this.body.blocked.left) {
-        this.body.velocity.x = Spider.SPEED; // turn right
+        this.body.velocity.x = Barrel.SPEED; // turn right
     }
 };
 
-Spider.prototype.die = function () {
+Barrel.prototype.die = function () {
     this.body.enable = false;
 
     this.animations.play('die').onComplete.addOnce(function () {
