f15cb64035e62f81bc74407201571f31039187cc
Weiskopfseeadler <Weiskopfseeadler@users.noreply.github.com>
2019-01-31 11:15:56 +0100

    ♥♥♥♥

--- js/Barrel.js
+++ js/Barrel.js
@@ -6,52 +6,42 @@ function Barrel(game, x, y) {
     Phaser.Sprite.call(this, game, x, y, 'barrel');
     // anchor
     this.anchor.set(0.5);
-    // animation
-    this.animations.add('roll', [0, 2, 4], 8, true); // 8fps, looped
-    this.animations.play('roll');
-    // physic properties
+     
     this.game.physics.enable(this);
     this.body.collideWorldBounds = true;
     
     this.body.velocity.x = Barrel.SPEED;
     this.ROLLSPEED = Barrel.ROLLSPEED;
-    //console.log(this);
+ 
 }
 
 
-
-// inherit from Phaser.Sprite
 Barrel.prototype = Object.create(Phaser.Sprite.prototype);
 Barrel.prototype.constructor = Barrel;
 
-Barrel.prototype.changeDirection = function () {
-    // check against walls and reverse direction if necessary
-  
+Barrel.prototype.changeDirection = function () {    
         this.ROLLSPEED*= -1;
         this.body.velocity.x *= -1;
        };
 
 
 Barrel.prototype.update = function () {
-    // check against walls and reverse direction if necessary
-    
+   
     
     if (this.body.touching.right || this.body.blocked.right) {
-        this.body.velocity.x = -Barrel.SPEED; // turn left        
+        this.body.velocity.x = -Barrel.SPEED;      
         this.ROLLSPEED *= -1; 
-       // console.log("CH")
+       
     }
     else if (this.body.touching.left || this.body.blocked.left) {
-        this.body.velocity.x = Barrel.SPEED; // turn right
-        this.ROLLSPEED *= -1;
-        //console.log("CH")
+        this.body.velocity.x = Barrel.SPEED; 
+        this.ROLLSPEED *= -1;    
     }
 };
 
 Barrel.prototype.die = function () {
     this.body.enable = false;
-
-    this.animations.play('die').onComplete.addOnce(function () {
-        this.kill();
-    }, this);
+ 
+     this.kill();
+   
 };
--- js/main.js
+++ js/main.js
@@ -4,8 +4,6 @@ PlayState = {};
 
 const LEVEL_COUNT = 3;
 
-PlayState.timer;
-
 PlayState.init = function (data) {
     this.game.renderer.renderSession.roundPixels = true;
 
@@ -23,7 +21,7 @@ PlayState.init = function (data) {
     }, this);
 
     
-    this.hasKey = false;
+  
     this.level = (data.level || 0) % LEVEL_COUNT;
 };
 
@@ -31,46 +29,32 @@ PlayState.init = function (data) {
 
 PlayState.preload = function () {
 
-    this.game.load.json('level:2', 'data/level03.json');
-    this.game.load.json('level:1', 'data/level00.json');	    this.game.load.json('level:1', 'data/level01.json');
-    this.game.load.json('level:0', 'data/level01.json');	    this.game.load.json('level:2', 'data/level00.json');
+    this.game.load.json('level:0', 'data/level03.json');
+    this.game.load.json('level:2', 'data/level00.json');	 
+    this.game.load.json('level:1', 'data/level01.json');	 
 
-
-    this.game.load.image('font:numbers', 'images/numbers.png');
 
     this.game.load.image('background', 'images/pixel-castle-background.png');
     this.game.load.image('dk', 'images/DoKo.png');
-
     this.game.load.image('ground', 'images/ground.png');
     this.game.load.image('invisible-wall', 'images/invisible_wall.png');
-    this.game.load.image('icon:coin', 'images/coin_icon.png');
-
     this.game.load.image('row','images/platform.png',1,1);
     this.game.load.image('ladder','images/Ladder.png');
     this.game.load.image('barrel','images/Barrel.png');
     this.game.load.image('flag','images/FinishFlag.png');
-
-    this.game.load.spritesheet('coin', 'images/coin_animated.png', 22, 22);
-
     this.game.load.spritesheet('hero', 'images/MarioWalk.png', 47, 64);
-    this.game.load.spritesheet('door', 'images/door.png', 65, 50);
 
 
     this.game.load.audio('sfx:jump', 'audio/jump.wav');
-    this.game.load.audio('sfx:coin', 'audio/coin.wav');
     this.game.load.audio('sfx:stomp', 'audio/stomp.wav');
-    
-    this.game.load.audio('sfx:door', 'audio/door.wav');
+
 };
 
 PlayState.create = function () {
     // create sound entities
     this.sfx = {
         jump: this.game.add.audio('sfx:jump'),
-        coin: this.game.add.audio('sfx:coin'),
         stomp: this.game.add.audio('sfx:stomp'),
-        key: this.game.add.audio('sfx:key'),
-        door: this.game.add.audio('sfx:door')
     };
 
     // create level
@@ -88,21 +72,7 @@ PlayState.update = function () {
     this._handleInput();
 };
 
-PlayState._handleCollisions = function () {
-    //this.game.physics.arcade.collide(this.barrels, this.platforms);
-    //this.game.physics.arcade.collide(this.barrels, this.enemyWalls);
-    this.game.physics.arcade.collide(this.hero, this.platforms);
 
-    this.game.physics.arcade.collide(this.barrels, this.platforms);
-   // this.game.physics.arcade.collide(this.barrels, this.enemyWalls,this._onEnemyVsWall,this,null);
-    this.game.physics.arcade.overlap(this.hero, this.Flag,  this._onHeroVsFinishFlag,
-        null, this);
-    this.game.physics.arcade.overlap(this.hero,this.ladders,this._onHeroVsLadder,null,this);
-
-    this.game.physics.arcade.overlap(this.hero, this.barrels,
-        this._onHeroVsEnemy, null, this);
-
-};
 
 
 PlayState._handleInput = function () {
@@ -121,26 +91,22 @@ PlayState._loadLevel = function (data) {
     // create all the groups/layers that we need
     this.bgDecoration = this.game.add.group();
     this.platforms = this.game.add.group();
-    this.coins = this.game.add.group();
     this.barrels = this.game.add.group();
     this.enemyWalls = this.game.add.group();
     this.Barrelspawns = this.game.add.group();
-    this.ladders = this.game.add.group();
-     
+    this.ladders = this.game.add.group();     
     this.Flag = this.game.add.group();
-    this.enemyWalls.visible = false;
-
-    // spawn all platforms
+    this.enemyWalls.visible = true;
     
-    data.platforms.forEach(this._spawnPlatform, this);
-    // spawn hero and enemies
-    this._spawnCharacters({hero: data.hero});
-   // this._spawnBarrels({barrels: data.barrels});
-    // spawn important objects
+ 
 
-    //this._spawnDK(data.DK.x, data.DK.y);
+    this._spawnCharacters({hero: data.hero});
+
+
+    this._spawnDK(data.DK.x, data.DK.y);
     this._spawnFlag(data.Flag.x, data.Flag.y);
-    data.Barrelspawns.forEach(this._barrelspawnspawn,this);
+    data.platforms.forEach(this._spawnPlatform, this);
+    data.Barrelspawns.forEach(this._barrelspawnspawn,this);    
     data.ladders.forEach(this._spawnLadders,this);
     data.enemyWalls.forEach(this._spawnEnemyWall,this);
     
@@ -166,8 +132,6 @@ PlayState._spawnLadders = function (ladder) {
     let sprite = this.ladders.create(
         ladder.x, ladder.y, ladder.image);
 
-    this._spawnEnemyWall(ladder.x, ladder.y, 'left');
-    this._spawnEnemyWall(ladder.x + sprite.width, ladder.y, 'right');
 };
 
 PlayState._barrelspawnspawn = function(Barelspawn){
@@ -178,9 +142,6 @@ PlayState._barrelspawnspawn = function(Barelspawn){
 
 };
 
-t = function() {
-    //console.log("Hallo");
-};
 
 PlayState._spawnPlatform = function (platform) {
     let sprite = this.platforms.create(
@@ -193,6 +154,7 @@ PlayState._spawnPlatform = function (platform) {
 
 
 };
+
 
 PlayState._spawnEnemyWall = function (x, y, side) {
     let sprite = this.enemyWalls.create(x, y, 'invisible-wall');
@@ -248,25 +210,33 @@ PlayState._spawnCharacters = function (data) {
     this.game.add.existing(this.hero);
 };
 
-PlayState._spawnCoin = function (coin) {
-    let sprite = this.coins.create(coin.x, coin.y, 'coin');
-    sprite.anchor.set(0.5, 0.5);
 
-    this.game.physics.enable(sprite);
-    sprite.body.allowGravity = false;
 
-    sprite.animations.add('rotate', [0, 1, 2, 1], 6, true); // 6fps, looped
-    sprite.animations.play('rotate');
+
+
+PlayState._handleCollisions = function () {
+
+    this.game.physics.arcade.collide(this.hero, this.platforms);
+
+    this.game.physics.arcade.collide(this.barrels, this.platforms);
+
+    this.game.physics.arcade.collide(this.barrels, this.enemyWalls,this._onBarrelVsWall,null,this);
+
+
+    this.game.physics.arcade.overlap(this.hero, this.Flag,  this._onHeroVsFinishFlag,
+        null, this);
+    this.game.physics.arcade.overlap(this.hero,this.ladders,this._onHeroVsLadder,null,this);
+
+    this.game.physics.arcade.overlap(this.hero, this.barrels,
+        this._onHeroVsEnemy, null, this);
+
 };
 
 
-
-
-
-
-PlayState._onHeroVsEnemy = function (hero, enemy) {
+PlayState._onHeroVsEnemy = function (hero, barrel) {
     if (hero.body.velocity.y > 0) { // kill enemies when hero is falling
         hero.bounce();
+        barrel.die();
         this.sfx.stomp.play();
     }
     else { // game over -> restart the game
@@ -280,24 +250,22 @@ PlayState._onHeroVsLadder = function (hero, ladder) {
     //
     if(this.hero.x == ladder.x){
         this.hero.x = ladder.x;
-        hero.climb(true);
+        hero.climb();
     }
     
 };
 
 PlayState._onHeroVsFinishFlag = function () {
-    //console.log(this.level);
+    
     this.game.state.restart(true, false, { level: this.level + 1 });
 };
 
-PlayState._onEnemyVsWall=function (enemy,wall) {
-    enemy.kill();
-}
+PlayState._onBarrelVsWall = function (hero, barrel) {
+    
+  barrel.die();
 
+};
 
-// =============================================================================
-// entry point
-// =============================================================================
 
 window.onload = function () {
     let game = new Phaser.Game(1200, 890, Phaser.AUTO, 'game');
